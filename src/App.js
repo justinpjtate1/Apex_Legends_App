714859a5896ca1b472313d3d5b9b7c913178cc41
Leon Cem <lca.ldn@gmail.com>
2023-02-10 16:03:29 +0000

    readded nonisaurus changes

--- src/App.js
+++ src/App.js
@@ -72,6 +72,23 @@ class App extends Component{
     })
   }
 
+// GENERAL CHAT
+  getGeneralChat = () => {
+    axios.get(`${apiUrl}/api/generalchat`, {
+      headers: {
+          Authorization: `Bearer ${localStorage.getItem("jwt")}`
+      }
+    })
+    .then((response) => {
+        return response.data
+    }).then((results) => {
+      console.log(results)
+        this.setState({
+          comments: results
+        })
+    })
+  }
+
   render() {
     return(
       <>
@@ -88,7 +105,7 @@ class App extends Component{
             <Routes>
               <Route path="/api/user" element={this.state.auth ? (<Profile />) : (<Navigate replace to = {"/"} />)} />
               <Route path="/api/weapons" element={this.state.auth ? (<Weapons weapons={this.state.weapons}/>) : (<Navigate replace to = {"/"} />)} />
-              <Route path="/api/generalchat" element={this.state.auth ? (<GeneralChat />) : (<Navigate replace to = {"/"} />)} />
+              <Route path="/api/generalchat" element={this.state.auth ? (<GeneralChat generalChat={this.getGeneralChat} comments={this.state.comments}/>) : (<Navigate replace to = {"/"} />)} />
               <Route path="/api/signin" element={!this.state.auth ? (<Signin userSignedIn={() => this.userSignedIn()}/>) : (<Navigate replace to = {"/"} />)}/>
               <Route path="/api/signup" element={!this.state.auth ? (<Signup />) : (<Navigate replace to = {"/"} />)}/>
               <Route path="/" element={!this.state.auth ? (<h1>Welcome to this Apex Legends App!</h1>) : (<h1>We will set this to be the page name</h1>)}/>
