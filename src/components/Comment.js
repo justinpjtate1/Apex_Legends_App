5efaf752ef528539f162342f534a0a274ea25039
norazics <nora.zics@yahoo.co.uk>
2023-02-12 23:33:37 +0000

    axios post comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
-function Comment(props){
-    return(
-        <div>
-            <h1>comment</h1>
-        </div>
-    )} 
+class Comment extends Component{
+    render() {
+        return(
+            <div>
+                <Card>
+                    <Card.Header>{this.props.username}</Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                        {this.props.comment}
+                        </Card.Text>
+                        <Button variant="primary">Edit</Button>
+                        <Button variant="primary">Delete</Button>
+                        <Button variant="primary">Update</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+        )} 
+    }
 
 
 export default Comment
--- src/components/GeneralChat.js
+++ src/components/GeneralChat.js
@@ -1,15 +1,104 @@
 import React, { Component } from "react"
-import axios from "axios"
-import apiUrl from "../apiConfig"
 import Comment from "./Comment"
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import axios from "axios";
+import apiUrl from "../apiConfig";
 
-function GeneralChat(props) {
-    return(
-        <div>
-            <h1> General Chat </h1>
-            <Comment />
-        </div>
-    )} 
+class GeneralChat extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            comment: '',
+            allComments: []
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    // 
+    getGeneralChat = () => {
+        axios.get(`${apiUrl}/api/generalchat`, {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwt")}`
+        }
+        })
+        .then((response) => {
+            return response.data
+        }).then((results) => {
+        console.log(results)
+            this.setState({
+                allComments: results
+            })
+        })
+    }
+
+
+
+    handleChange (event) {
+        console.log('hey')
+        this.setState({
+            comment: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        console.log(event)
+        event.preventDefault();
+
+        console.log(localStorage.getItem("jwt"))
+        // saving it 
+        axios.post(`${apiUrl}/api/generalchat`, {
+            comment: this.state.comment
+          }, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("jwt")}`
+            },
+          })
+          .then((response) => {
+              return response.data
+          })
+
+
+        
+      }
+    addNewComment = () => { 
+        this.state.allComments.map((comment) => {
+        return <Comment 
+        username={comment.username}
+        comment={comment.comment}/>
+    })}
+
+
+    editComment() {
+
+    }
+
+    deleteComment() {
+
+    }
+
+    updateComment() {
+
+    }
+
+    render() {
+
+        return(
+            <div>
+                <h1> General Chat </h1>
+                <Card>
+                    <Card.Header>Comment</Card.Header>
+                    <Card.Body>
+                        <input type="text" onChange={this.handleChange} />
+                        <Button variant="primary" type="submit" onClick={this.handleSubmit}>Submit</Button>
+                    </Card.Body>
+                </Card>
+               { this.addNewComment }
+            </div>
+        )
+    } 
+}
 
 
 export default GeneralChat
